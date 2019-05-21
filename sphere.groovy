d5fa0336b91e7e8faa9560639eedd4abe344d089
Kevin Harrington <mad.hephaestus@gmail.com>
2019-05-21 11:57:14 -0400

    smalle spheres

--- sphere.groovy
+++ sphere.groovy
@@ -1,8 +1,8 @@
 //create a sphere
-CSG sphere = new Sphere(100)// Spheres radius
+CSG sphere = new Sphere(25)// Spheres radius
 				.toCSG()// convert to CSG to display
 				.movex(200)
-CSG sphereHighRes = new Sphere(100,// Spheres radius
+CSG sphereHighRes = new Sphere(25,// Spheres radius
 						40,//elevation Divisions
 						40)// vertical divisions
 						.toCSG()// convert to CSG to display
