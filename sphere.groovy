c63b0cfef6861918064cc13cc66359aafab98287
Kevin Harrington <mad.hephaestus@gmail.com>
2021-08-08 17:17:38 -0400

    whatever.SVG

--- sphere.groovy
+++ sphere.groovy
@@ -1,10 +1,10 @@
 //create a sphere
 CSG sphere = new Sphere(25)// Spheres radius
 				.toCSG()// convert to CSG to display
-				.movex(55)
+				
 CSG sphereHighRes = new Sphere(25,// Spheres radius
 						40,//elevation Divisions
 						40)// vertical divisions
 						.toCSG()// convert to CSG to display
-
+sphere=sphere.movex(55)
 return [sphere,sphereHighRes]
